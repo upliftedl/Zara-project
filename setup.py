d757fcaca6dee5363df2dca59f0f822ff94bc975
abhishek johns <abhishekjohns418@gmail.com>
2025-05-17 12:17:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,24 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["aiohttp", "lxml"],
+    install_requires=[
+        "aiohttp",
+        "lxml"
+    ],
     entry_points={
         "console_scripts": [
-            "zara=cli:main"
+            "zara=zara.cli:main"
         ]
+
     },
     author="upliftedl",
     description="Fast, extensible username enumeration tool",
-    license="MIT"
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8"
 )
